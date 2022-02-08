5daf9ccec3dc09ae187f33d268ecf9932398e957
hajeongryu <tgkwjdt@gmail.com>
2022-02-08 15:35:23 +0900

    이클립스 프로젝트 설정

---
+++ main/java/com/team/RhollApplication.java
@@ -0,0 +1,13 @@
+package com.team;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhollApplication.class, args);
+	}
+
+}
