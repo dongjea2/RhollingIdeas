dc373b3d2b4af9a61fe7628663404306e4d57303
2reesh <dltkdgns1125@gmail.com>
2022-02-10 16:30:48 +0900

    게시판 jpa수정

--- main/java/com/team/post/entity/Comments.java
+++ main/java/com/team/post/entity/Comments.java
@@ -3,12 +3,18 @@ package com.team.post.entity;
 
 
 import java.util.Date;
+import java.util.Objects;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
@@ -26,19 +32,25 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 @Entity(name="comment")
 @Table
 public class Comments {
-	@Column(name="comment_no")
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int commentNo;
+	
 	@ManyToOne
 	@JoinColumn(name="post_no")
 	private Post post;
+	
 	@ManyToOne
 	@JoinColumn(name="user_no")
 	private Customer maker;
+	
 	@Column(name="comment_content")
 	private String commentContent;
+	
+	@CreationTimestamp
 	@Column(name="comment_date")
-	@JsonFormat(pattern = "yy/MM/dd", timezone = "Asia/Seoul")
 	private Date commentDate;
+	
 	
 		
 	
--- main/java/com/team/post/entity/Post.java
+++ main/java/com/team/post/entity/Post.java
@@ -3,14 +3,21 @@ package com.team.post.entity;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.DynamicInsert;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
@@ -22,25 +29,30 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 	`post_date`	DATE	NULL,
 	`user_no`	NUMBER(8)	NOT NULL
 */
-@Entity(name="post")
-@Table
+@Entity
+@Table(name="post")
+@DynamicInsert
 public class Post {
-	@Column(name="post_no")
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int	postNo;
 	
 	@ManyToOne
 	@JoinColumn(name="porject_no")
 	private Project	project;
+	
+	@CreationTimestamp
 	@Column(name="post_date")
-	@JsonFormat(pattern = "yy/MM/dd", timezone = "Asia/Seoul")
 	private Date	postDate;
+	
 	@Column(name="post_content")
 	private String	postContent;
+	
 	@ManyToOne
 	@JoinColumn(name="user_no")
 	private Customer maker;
+	
 //	@OneToMany
-//	@JoinColumn(name="")
 //	private List<Comments> comments;
 	
 			
