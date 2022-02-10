fd091e52b29651b2b5e9a40437d0eefd856e04dd
whoshwj <whoshoo25@gmail.com>
2022-02-10 17:03:19 +0900

    인터레스트 엔터티 수정

--- main/java/com/team/interest/entity/Interest.java
+++ main/java/com/team/interest/entity/Interest.java
@@ -1,12 +1,16 @@
 package com.team.interest.entity;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+import com.team.project.entity.Project;
+import com.team.user.entity.Customer;
 
 @Entity
 @Table(name="interest")
@@ -37,6 +41,21 @@ public class Interest implements Serializable{
 	}
 	public void setInterestAlarm(String interestAlarm) {
 		this.interestAlarm = interestAlarm;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(likeProject, likeUser);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Interest other = (Interest) obj;
+		return Objects.equals(likeProject, other.likeProject) && Objects.equals(likeUser, other.likeUser);
 	}
 	
 	
