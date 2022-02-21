b41266f0a5e8fa8508eca70062abb53ff10e4566
Jihoon Oh <bs4079.dev@gmail.com>
2022-02-21 17:50:33 +0900

    CustomerController / CustomerService 중간커밋 참고하세요

--- main/java/com/team/user/controller/CustomerController.java
+++ main/java/com/team/user/controller/CustomerController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.team.user.entity.Customer;
@@ -19,20 +20,49 @@ public class CustomerController {
 	private CustomerService service;
 	
 	@PostMapping("/login")
-	public Object login(@RequestBody Customer customer) {
-		System.out.println(customer);
+	public Object login(@RequestBody Customer c) {
+		System.out.println(c);
 		Map<String, Object> returnMap = new HashMap<>();
-		Customer c = service.login(customer.getUserId(), customer.getUserPwd());
-		if(c==null) {
+		Customer c2 = service.login(c.getUserId(), c.getUserPwd());
+		if(c2==null) {
 			returnMap.put("status", 0);
 			returnMap.put("msg", "로그인 실패. 아이디나 비밀번호를 확인해주세요");
 		} else { //아이디와 비밀번호가 db와 일치
-			if(c.getUserStatus().equals("0")) { //회원정보는 일치하지만 계정탈퇴상태
+			if(c2.getUserStatus().equals("0")) { //회원정보는 일치하지만 계정탈퇴상태
 				returnMap.put("status", 2);
 				returnMap.put("msg", "탈퇴한 계정입니다");
 			} else {
 				returnMap.put("status", 1);
 			}
+		}
+		return returnMap;
+	}
+	@RequestMapping("/iddupchk")
+	public Object iddupchk(@RequestBody Customer c) {
+		Map<String, Object> returnMap = new HashMap<>();
+		Customer c2 = service.findByUserId(c.getUserId());
+		if(c2 != null) {
+			returnMap.put("status", 0);
+			returnMap.put("msg", "이미 존재하는 아이디입니다");
+		} else {
+			returnMap.put("status", 1);
+			returnMap.put("msg", "사용할 수 있는 아이디입니다");
+		}
+		return returnMap;
+	}
+	
+	@PostMapping("/signup")
+	public Object signup(@RequestBody Customer c) {
+		System.out.println(c.getUserId() + " " +c.getUserPwd() + " " +c.getUserName());
+		Map<String, Object> returnMap = new HashMap<>();
+		Customer c2 = service.findByUserId(c.getUserId());
+		if(c2 == null) {
+			service.signup(c.getUserName(), c.getUserId(), c.getUserPwd());
+			returnMap.put("status", 1);
+			returnMap.put("msg", "가입성공");
+		} else {
+			returnMap.put("status", 0);
+			returnMap.put("msg", "가입실패");
 		}
 		return returnMap;
 	}
--- main/java/com/team/user/service/CustomerService.java
+++ main/java/com/team/user/service/CustomerService.java
@@ -3,7 +3,6 @@ package com.team.user.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.team.exception.FindException;
 import com.team.user.entity.Customer;
 import com.team.user.repository.CustomerRepository;
 
@@ -13,11 +12,24 @@ public class CustomerService {
 	@Autowired
 	CustomerRepository customerRepo;
 	
+	public Customer findByUserNo(int userNo) {
+		return customerRepo.findByUserNo(userNo);
+	}
+	
+	public Customer findByUserId(String userId) {
+		return customerRepo.findByUserId(userId);
+	}
+	
 	public Customer login(String userId, String userPwd) {
 		Customer c = customerRepo.findByUserId(userId);
 		if(c!=null && c.getUserPwd().equals(userPwd)) {
 			return c;
 		}
 		return null;
+	}
+	
+	public void signup(String userName, String userId, String userPwd) {
+		Customer c2 = new Customer(userName, userId, userPwd);
+		customerRepo.save(c2);
 	}
 }
