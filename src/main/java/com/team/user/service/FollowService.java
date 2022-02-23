bf4ec79ae3da06d8c8feef7bd6a12ea339cc7bb5
hajeongryu <tgkwjdt@gmail.com>
2022-02-23 17:11:35 +0900

    follow 수정 및 생성

--- src/main/java/com/team/user/service/FollowService.java
+++ src/main/java/com/team/user/service/FollowService.java
@@ -1,12 +1,24 @@
 package com.team.user.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.team.user.entity.Customer;
+import com.team.user.entity.Follow;
 import com.team.user.repository.FollowRepository;
 
 @Service
 public class FollowService {
 	@Autowired
 	private FollowRepository repository;
+	
+	public List<Follow> following(Customer c) {
+		return repository.findByUserNo(c);
+	}
+	
+	public List<Follow> followers(Customer c) {
+		return repository.findByFollow(c);
+	}
 }
