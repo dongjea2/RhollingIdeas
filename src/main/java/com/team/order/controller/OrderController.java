ebe54b7873574d5c6ca5b18687ebee56eb917ff5
dongjea2 <dewawd@gmail.com>
2022-02-24 12:38:29 +0900

    오더 컨트롤러 @PostMapping("/order") 추가

--- src/main/java/com/team/order/controller/OrderController.java
+++ src/main/java/com/team/order/controller/OrderController.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,4 +33,33 @@ public class OrderController {
 		List<Order> list = service.myOrderProjects(c.getUserNo());
 		return list;
 	}
+	
+	@PostMapping("/order")
+	public Object orderReward(@RequestBody Order order) {
+		Map<String, Object> returnMap = new HashMap<>();
+		
+		System.out.println("최종 가격 : "+order.getExtraPrice());
+		System.out.println("프로젝트 번호 : "+order.getProject().getProjectNo());
+		System.out.println("구매자 ID : "+order.getOrderUser().getUserNo());
+		
+		order.setOrderResult("진행중");
+
+		if( 1 != order.getOrderUser().getUserNo()) {
+			returnMap.put("status", 0);
+			returnMap.put("msg", "구매 실패, 다시 로그인 해주세요");
+			return returnMap;
+		} 
+		
+		try {
+			service.add(order);
+			returnMap.put("status", 1);
+			returnMap.put("msg", "구매 성공");
+
+		} catch (FindException e) {
+			returnMap.put("status", 0);
+			returnMap.put("msg", "구매 실패, 잘못된 상품 정보");
+		}
+			return returnMap;
+	}
+	
 }
