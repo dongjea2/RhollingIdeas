dddb1daca64202bcae215ffbeb8238449d463a2e
2reesh <dltkdgns1125@gmail.com>
2022-02-24 12:37:38 +0900

    프로젝트 작성 카테고리부분 완성

--- src/main/java/com/team/project/controller/ProjectController.java
+++ src/main/java/com/team/project/controller/ProjectController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.team.project.entity.Category;
 import com.team.project.entity.Project;
 import com.team.project.entity.Reward;
 import com.team.project.service.ProjectService;
@@ -51,8 +52,16 @@ public class ProjectController {
 //		if(c != null) {
 //
 //		}
+				
 		List<Project> list = service.createdProject(c);
+		
 		return list;
+	}
+	
+	@GetMapping("/category")
+	public List<Category> categorylist(){
+		List<Category> category = service.findAllCategory();
+		return category;
 	}
 	
 	
