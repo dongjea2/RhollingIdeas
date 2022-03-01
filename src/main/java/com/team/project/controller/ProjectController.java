286d4890db7483260ec50f3bfdfd56097ad33d40
2reesh <dltkdgns1125@gmail.com>
2022-03-01 23:14:49 +0900

    프로젝트 컨트롤러 충돌병합

--- src/main/java/com/team/project/controller/ProjectController.java
+++ src/main/java/com/team/project/controller/ProjectController.java
@@ -14,10 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-<<<<<<< HEAD
-=======
 import org.springframework.web.bind.annotation.RequestParam;
->>>>>>> bca8ce5c5f41529d457ed17cca12a65e7eb81016
 import org.springframework.web.bind.annotation.RestController;
 
 import com.team.project.dto.CreatedProjectDTO;
