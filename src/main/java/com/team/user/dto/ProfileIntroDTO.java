21d6a2683acea18277f837247d5fa9d019ff7d46
hajeongryu <tgkwjdt@gmail.com>
2022-03-02 06:23:44 +0900

    프로필 팔로우 체크 추가

--- src/main/java/com/team/user/dto/ProfileIntroDTO.java
+++ src/main/java/com/team/user/dto/ProfileIntroDTO.java
@@ -8,11 +8,14 @@ public class ProfileIntroDTO {
 	private String userName;
 	private String userImage;
 	private String userIntroduction;
+	private String userUrl;
 	
 	private int orderProjectCnt;
 	private int createdProjectCnt;
 	private int followerCnt;
 	private int followingCnt;
+	
+	private boolean followCheck;
 	
 	public void entityToDTO(Customer c) {
 		this.userNo = c.getUserNo();
@@ -20,6 +23,7 @@ public class ProfileIntroDTO {
 		this.userName = c.getUserName();
 		this.userImage = c.getUserImage();
 		this.userIntroduction = c.getUserIntroduction();
+		this.userUrl = c.getUserUrl();
 	}
 
 	public void setOrderProjectCnt(int orderProjectCnt) {
@@ -35,6 +39,14 @@ public class ProfileIntroDTO {
 		this.followingCnt = followingCnt;
 	}
 	
+	public boolean isFollowCheck() {
+		return followCheck;
+	}
+
+	public void setFollowCheck(boolean followCheck) {
+		this.followCheck = followCheck;
+	}
+
 	public int getUserNo() {
 		return userNo;
 	}
@@ -49,6 +61,9 @@ public class ProfileIntroDTO {
 	}
 	public String getUserIntroduction() {
 		return userIntroduction;
+	}
+	public String getUserUrl() {
+		return userUrl;
 	}
 	public int getOrderProjectCnt() {
 		return orderProjectCnt;
